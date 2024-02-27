cb03cf7fb1b082ae680a447c39788828cb71767e
MahmoudFawzyAbdelmonem <159821848+MahmoudFawzyAbdelmonem@users.noreply.github.com>
2024-02-27 16:42:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,159 +1,58 @@
+var box = document.querySelector(".rocket")
+var boxLeft = 0
+var boxRight = 0 
+var boxUp = 0
+var boxBottom = 0
 
-// 1-
-// var x = prompt("enter the num")
-// if (x %  3 ==0 && x % 4 ==0) {
-//     console.log("yes");
-// }else{
-//     console.log("no");
-// }
-
-// 2-
-//  var x = prompt("enter num1")
-//  var y = prompt("enter num2")
-
-//  if( x > y ){
-//     console.log(x);
-//  } else{
-//     console.log(y);
-//  }
-
-// 3-
-
-// var x = prompt("enter num1")
-
-
-// if( x < 0  ){
-//    console.log("negative");
-// } else{
-//     console.log("positive");
-// }
-
-// 4-
-
-// var x = prompt(" num1")
-// var y = prompt("num2 ")
-// var z = prompt(" num3 ")
-// if (x > y && x > z) {
-//     console.log("max num" + x ,"min num" + z );
-    
-// }else if(x > z && z > y){
-//     console.log("max num" + x );
-//     console.log("min num" + y); 
-// } else if (y > x && x > z){
-//     console.log("max num" + y );
-//     console.log("min num" + z);
-// } else if(y > z && z > x){
-//     console.log("max num" + y );
-//     console.log("min num" + x);
-// } 
-//  else if(z > x && x > y){
-//     console.log("max num" + z );
-//     console.log("min num" + y);
-// }else if(z > y && y > x){
-//     console.log("max num" + z );
-//     console.log("min num" + x);
-// }
+document.addEventListener ("keydown" , function(e){
+    // console.log(e);
+    if (e.code == 'ArrowRight') {
+        boxRight += 100
+        box.style.left = boxRight + 'px'
+    }else if (e.code == 'ArrowLeft') {
+        boxRight -= 100
+        box.style.left = boxRight + 'px'
+    }else if (e.code == 'ArrowUp'){
+        boxUp -= 100
+        box.style.top = boxUp + 'px'
+    }else if (e.code == 'ArrowDown') {
+        boxUp += 100
+        box.style.top = boxUp+ 'px'
+    }
 
 
-// 5-
-// var x = prompt ("enter the num")
+})
 
-// if (x % 2 == 0) {
-//     console.log('even');
-// } else {
-//     console.log('odd');
-// }
-// 6-
-// var x = prompt("enter a word")
+var smallBox = document.querySelector (".shot")
 
-// if (x[0] == "a" || x[0] =="e" || x[0] =="i" || x[0] =="o" || x[0] =="u" ) {
-//     console.log("vowel" );
-// }else{
-//     console.log("consonant" );
-// }
-// 7-
-// var x = prompt(" num")
+document.addEventListener ("keydown" , function (e) {
+    if(e.code == 'Space') { 
+        smallBox.style.opacity = 3
+        smallBox.style.bottom =700 + 'px'
+        smallBox.style.transition= 1  +'s'
+    } 
+})
+ document.addEventListener('keyup', function (e) {
+    if(e.code == 'Space') {
+        smallBox.style.opacity= 0
+        smallBox.style.bottom = 0+'px'
+        smallBox.style.transition= 0 +'s'
+    }
+ })
 
-// for(i =1; i <= x; i++){
-//     console.log(i);
-// }
-// 8-
-// var x = prompt("please enter a number ") 
+ var smallBox1 = document.querySelector (".shot1")
 
-// for (i = 1 ; i <=12 ; i ++ ){
-//     console.log( x + '*' + i +"=" +(x*i))
-// }
-
-// 9-
-// var x = prompt("please enter a number ") 
-// for (i = 2 ; i < x  ; i+=2 ){
-//     console.log(i);
-// }
-
-// 10-
-// var x = prompt ("num1")
-// var y = prompt ("num2")
-// console.log(x**y);
-
-// 11-
-//  var x = +prompt("enter your mark1")
-//  var y = +prompt("enter your mark2")
-//  var z = +prompt("enter your mark3")
-//  var v = +prompt("enter your mark4")
-//  var n = +prompt("enter your mark5")
-
-//  console.log(x + y + z + v + n);
-//  console.log((x + y + z + v + n) / 5);
-//  console.log((x + y + z + v + n)/ 500 *100);
-
-// 12-
-// var x = prompt ("enter number of month ")
-
-// if(x == 1){
-//     console.log(31 + "days" );
-// } else if  (x == 2) {
-//     console.log("29 days or 28 days" );
-// }else if  (x == 3) {
-//     console.log(31 + "days" );
-// }else if (x == 4) {
-//     console.log(30 + "days" );
-// }else if (x == 5) {
-//     console.log(31 + "days" );
-// }else if (x == 6) {
-//     console.log(30 + "days" );
-// }else if (x == 7) {
-//     console.log(31 + "days" );
-// }else if (x == 8) {
-//     console.log(31 + "days" );
-// }else if (x == 9) {
-//     console.log(30 + "days" );
-// }else if (x == 10) {
-//     console.log(31 + "days" );
-// }else if (x == 11) {
-//     console.log(30 + "days" );
-// }else if (x == 12) {
-//     console.log(31 + "days" );
-// }
-
-// 13-
-var x = +prompt("enter your mark in Physics")
- var y = +prompt("enter your mark in Chemistry ")
- var z = +prompt("enter your mark in Biology")
- var v = +prompt("enter your mark in Mathmatics")
- var n = +prompt("enter your mark in Computer")
-
- if(((x + y + z + v + n)/500 *100) >= 90  ){
-    console.log( (x + y + z + v + n)/500 *100 + "%" + ": Grad A");
- }else if(((x + y + z + v + n)/500 *100) >= 80 && ((x + y + z + v + n)/500 *100)<90 ){   
-    console.log( (x + y + z + v + n)/500 *100 + "%" +  ": Grad B");
- }else if(((x + y + z + v + n)/500 *100) >= 70 && ((x + y + z + v + n)/500 *100) < 80 ){
-    console.log( (x + y + z + v + n)/500 *100 + "%" +  " : Grad C");
- }else if(((x + y + z + v + n)/500 *100) >= 60 && ((x + y + z + v + n)/500 *100)< 70 ){
-    console.log( (x + y + z + v + n)/500 *100 + "%" +  ": Grad D");
- }else if(((x + y + z + v + n)/500 *100) >= 50 && ((x + y + z + v + n)/500 *100)< 60 ){
-    console.log( (x + y + z + v + n)/500 *100 + "%" +  " : Grad E");
- }else if(((x + y + z + v + n)/500 *100) <= 40 ){
-    console.log( (x + y + z + v + n)/500 *100 +  "%" + " : Grad F");
- }
-
-
+document.addEventListener ("keydown" , function (e) {
+    if(e.code == 'Space') { 
+        smallBox1.style.opacity = 3
+        smallBox1.style.bottom =700 + 'px'
+        smallBox1.style.transition= 1  +'s'
+    } 
+})
+ document.addEventListener('keyup', function (e) {
+    if(e.code == 'Space') {
+        smallBox1.style.opacity= 0
+        smallBox1.style.bottom = 0+'px'
+        smallBox1.style.transition= 0 +'s'
+    }
+ })
